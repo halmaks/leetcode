4f1a874bc1c288b32c56d30ce6b31827ce9ef7e5
halmaks <halatsiankou@gmail.com>
2020-09-09 03:39:12 +0300

    bug with 1960 is fixed

--- src/main/java/com/halmaks/leetcode/Solution2.java
+++ src/main/java/com/halmaks/leetcode/Solution2.java
@@ -2,14 +2,72 @@ package com.halmaks.leetcode;
 
 public class Solution2 {
     /*
+     *  Given an array of 4 digits, return the largest 24 hour time that
+     *  can be made.
+     *  The smallest 24 hour time is 00:00, and the largest is 23:59.
+     *  Starting from 00:00, a time is larger if more time has elapsed
+     *  since midnight.
+     *  Return the answer as a string of length 5.  If no valid time can
+     *  be made, return an empty string.
      *  arr.length == 4
      *  0 <= arr[i] <= 9
      */
-    public String largestTimeFromDigits(int[] arr){
-        String time = "";
+    public String largestTimeFromDigits(int[] arr) {
+        final int HOURS_MAX_VALUE = 23;
+        final int MINUTES_MAX_VALUE = 59;
+        int tmp = 0;
+        int max = -1;
 
+        int[][] algorithm = new int[][]{
+                {0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3},
+                {1, 0, 0, 0, 2, 2, 1, 1, 3, 3, 3, 2}
+        };
+        int indexOne = 0;
+        int indexTwo = 0;
 
+        for (int i = 0; i < 12; i++) {
+            tmp = arr[algorithm[0][i]] * 10 + arr[algorithm[1][i]];
+            if (tmp >= max && tmp <= HOURS_MAX_VALUE) {
+                max = tmp;
+                indexOne = algorithm[0][i];
+                indexTwo = algorithm[1][i];
+            }
+        }
 
-        return time;
+        String hours;
+        if (max < 0) {
+            return "";
+        } else if (max < 10) {
+            hours = "0" + max;
+        } else {
+            hours = "" + max;
+        }
+
+        int[] minutesDigits = new int[2];
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i != indexOne && i != indexTwo) {
+                minutesDigits[j] = arr[i];
+                j++;
+            }
+        }
+        String minutes;
+        int minutesCaseOne = minutesDigits[0] * 10 + minutesDigits[1];
+        int minutesCaseTwo = minutesDigits[1] * 10 + minutesDigits[0];
+        if (minutesCaseOne >= minutesCaseTwo && minutesCaseOne <= MINUTES_MAX_VALUE) {
+            if (minutesCaseOne < 10) {
+                return hours + ":0" + minutesCaseOne;
+            } else {
+                return hours + ":" + minutesCaseOne;
+            }
+        }
+        if (minutesCaseTwo <= MINUTES_MAX_VALUE) {
+            if(minutesCaseTwo < 10) {
+                return hours + ":0" + minutesCaseTwo;
+            } else {
+                return hours + ":" + minutesCaseTwo;
+            }
+        }
+        return "";
     }
 }
--- src/test/java/com/halmaks/leetcode/Solution2Test.java
+++ src/test/java/com/halmaks/leetcode/Solution2Test.java
@@ -1,27 +1,71 @@
 package com.halmaks.leetcode;
 
 import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
 
-class Solution2Test extends TestCase {
-    @org.junit.jupiter.api.Test
-    void largestTimeFromDigitsTest1() {
-        Solution2 solution = new Solution2();
-        String resultTime = solution
-                .largestTimeFromDigits(new int[]{1, 4, 5, 1});
-        assertEquals("15:41", resultTime);
+public class Solution2Test {
+    @Test
+    public void largestTimeFromDigitsTest1() {
+        Solution2 solution2 = new Solution2();
+        String resultTime = solution2
+                .largestTimeFromDigits(new int[]{1, 2, 3, 4});
+        Assert.assertEquals("23:41", resultTime);
     }
-    @org.junit.jupiter.api.Test
-    void largestTimeFromDigitsTest2() {
-        Solution2 solution = new Solution2();
-        String resultTime = solution
-                .largestTimeFromDigits(new int[]{1, 4, 5, 1});
-        assertEquals("15:41", resultTime);
+    @Test
+    public void largestTimeFromDigitsTest2() {
+        Solution2 solution2 = new Solution2();
+        String resultTime = solution2
+                .largestTimeFromDigits(new int[]{5, 5, 5, 5});
+        Assert.assertEquals("", resultTime);
     }
-    @org.junit.jupiter.api.Test
-    void largestTimeFromDigitsTest3() {
-        Solution2 solution = new Solution2();
-        String resultTime = solution
+    @Test
+    public void largestTimeFromDigitsTest3() {
+        Solution2 solution2 = new Solution2();
+        String resultTime = solution2
+                .largestTimeFromDigits(new int[]{0, 0, 0, 0});
+        Assert.assertEquals("00:00", resultTime);
+    }
+    @Test
+    public void largestTimeFromDigitsTest4() {
+        Solution2 solution2 = new Solution2();
+        String resultTime = solution2
+                .largestTimeFromDigits(new int[]{9, 1, 6, 8});
+        Assert.assertEquals("", resultTime);
+    }
+    @Test
+    public void largestTimeFromDigitsTest5() {
+        Solution2 solution2 = new Solution2();
+        String resultTime = solution2
                 .largestTimeFromDigits(new int[]{1, 4, 5, 1});
-        assertEquals("15:41", resultTime);
+        Assert.assertEquals("15:41", resultTime);
+    }
+    @Test
+    public void largestTimeFromDigitsTest6() {
+        Solution2 solution2 = new Solution2();
+        String resultTime = solution2
+                .largestTimeFromDigits(new int[]{0, 0, 3, 0});
+        Assert.assertEquals("03:00", resultTime);
+    }
+    @Test
+    public void largestTimeFromDigitsTest7() {
+        Solution2 solution2 = new Solution2();
+        String resultTime = solution2
+                .largestTimeFromDigits(new int[]{1, 9, 6, 0});
+        Assert.assertEquals("19:06", resultTime);
+    }
+    @Test
+    public void largestTimeFromDigitsTest8() {
+        Solution2 solution2 = new Solution2();
+        String resultTime = solution2
+                .largestTimeFromDigits(new int[]{1, 1, 1, 1});
+        Assert.assertEquals("11:11", resultTime);
+    }
+    @Test
+    public void largestTimeFromDigitsTest9() {
+        Solution2 solution2 = new Solution2();
+        String resultTime = solution2
+                .largestTimeFromDigits(new int[]{7, 3, 6, 0});
+        Assert.assertEquals("07:36", resultTime);
     }
 }
