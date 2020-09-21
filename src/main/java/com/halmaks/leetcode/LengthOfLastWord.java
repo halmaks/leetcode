62bfe20efe4d5686d5af1612a5408865d5155f59
halmaks <halatsiankou@gmail.com>
2020-09-22 00:12:53 +0300

    added test cases and some improvements

--- src/main/java/com/halmaks/leetcode/LengthOfLastWord.java
+++ src/main/java/com/halmaks/leetcode/LengthOfLastWord.java
@@ -2,23 +2,28 @@ package com.halmaks.leetcode;
 
 public class LengthOfLastWord {
 
-  public int lengthOfLastWord(String s) {
-    char [] c = s.toCharArray();
-    
-    if(c.length == 0) {
-      return 0;
+    public int lengthOfLastWord(String s) {
+        char[] c = s.toCharArray();
+
+        if (c.length == 0) {
+            return 0;
+        }
+
+        int count = 0;
+        int spaces = 0;
+
+        for (int i = 1; i <= c.length; i++) {
+            if (c[c.length - i] == ' ') {
+                if (i == 1 || spaces > 0) {
+                    spaces++;
+                    continue;
+                }
+                return count - spaces;
+            }
+            count = i;
+        }
+
+        return count - spaces;
     }
-    
-    int count = 0;
-    
-    for(int i = c.length - 1; i > -1; i--) {
-      if(c[i] == ' ') {
-        return c.length - i - 1;
-      }
-      count = i;
-    }
-    
-    return count;
-  }
 
 }
--- src/test/java/com/halmaks/leetcode/LengthOfLastWordTest.java
+++ src/test/java/com/halmaks/leetcode/LengthOfLastWordTest.java
@@ -11,7 +11,42 @@ public class LengthOfLastWordTest{
     }
     @Test
     public void lengthOfLastWordCase2() {
-        assertEquals(0, new LengthOfLastWord()
+        assertEquals(2, new LengthOfLastWord()
                 .lengthOfLastWord("hi"));
+    }
+    @Test
+    public void lengthOfLastWordCase3() {
+        assertEquals(1, new LengthOfLastWord()
+                .lengthOfLastWord("i"));
+    }
+    @Test
+    public void lengthOfLastWordCase4() {
+        assertEquals(2, new LengthOfLastWord()
+                .lengthOfLastWord("hi "));
+    }
+    @Test
+    public void lengthOfLastWordCase5() {
+        assertEquals(10, new LengthOfLastWord()
+                .lengthOfLastWord("withSpaces    "));
+    }
+    @Test
+    public void lengthOfLastWordCase6() {
+        assertEquals(0, new LengthOfLastWord()
+                .lengthOfLastWord(" "));
+    }
+    @Test
+    public void lengthOfLastWordCase7() {
+        assertEquals(0, new LengthOfLastWord()
+                .lengthOfLastWord("    "));
+    }
+    @Test
+    public void lengthOfLastWordCase8() {
+        assertEquals(4, new LengthOfLastWord()
+                .lengthOfLastWord("   word"));
+    }
+    @Test
+    public void lengthOfLastWordCase9() {
+        assertEquals(17, new LengthOfLastWord()
+                .lengthOfLastWord("  wordBetweenSpaces  "));
     }
 }
